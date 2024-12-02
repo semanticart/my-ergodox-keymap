62c9953cb9071cfa3993dca8d9ce1ab9309045d8
Jeffrey Chupp <jeff@semanticart.com>
2024-12-02 14:40:15 -0500

    Try older example

--- P5gLl/keymap.c
+++ P5gLl/keymap.c
@@ -188,6 +188,7 @@ void keyboard_post_init_user(void) {
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 // This globally defines all key overrides to be used
-const key_override_t *key_overrides[] = {
-	&delete_key_override
+const key_override_t **key_overrides = (const key_override_t *[]){
+	&delete_key_override,
+	NULL // Null terminate the array of overrides!
 };
